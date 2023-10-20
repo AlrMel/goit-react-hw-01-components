5ddfa40ed3282a87229a6b3983f1eef77dd7bdcb
ArlMel <alexandrm1233@gmail.com>
2023-10-20 12:56:24 +0300

    th02

--- src/components/transactionhistory/TransactionHistory.jsx
+++ src/components/transactionhistory/TransactionHistory.jsx
@@ -1,28 +1,27 @@
-import css from './TransactionHistory.module.css'
+import css from './TransactionHistory.module.css';
 
-export const TransactionHistory = () => {
-    return (
-        <table className={css["transaction-history"]}>
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css['transaction-history']}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-  <tbody>
-    <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
-    </tr>
-    <tr>
-      <td>Withdrawal</td>
-      <td>85</td>
-      <td>USD</td>
-    </tr>
-  </tbody>
-</table>
-    )
-}
+      <tbody>
+        {items.map(item => {
+          return (
+            <tr key={item.id}>
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td>{item.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
