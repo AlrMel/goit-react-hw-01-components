d5e1bdf156bb2f5f0529de6bc682b307ee892803
AlrMel <alexandrm1233@gmail.com>
2023-10-21 12:25:41 +0300

    ProfileCSS

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -7,23 +7,23 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
         <img src={avatar} 
         alt={username.name} 
         className={css.avatar} />
-        <p className={css.name}>Petra Marica</p>
-        <p className={css.tag}>@pmarica</p>
-        <p className={css.location}>Salvador, Brasil</p>
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{`@${tag}`}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
       <ul className={css.stats}>
         <li>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>1000</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>2000</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>3000</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
