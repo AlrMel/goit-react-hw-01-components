ea64b5a71d84316a7d0df7992e65e95eb32d1ff4
AlrMel <alexandrm1233@gmail.com>
2023-10-21 13:20:58 +0300

    stat-css-02

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,31 +1,26 @@
-import css from "./Statistics.module.css"
+import css from './Statistics.module.css';
 
 const randomColor = () => {
-    const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    return color;
-  };
+  const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return color;
+};
 
 export const Statistics = ({ title, stats }) => {
-    return (
-        <section className={css.statistics}>
+  return (
+    <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(item => (
-          <li key={item.id} 
-          className={css.item}>
-            <span 
-            className={css.label}>{item.label}</span>
-            <span
-              className={css.percentage}
-              style={{ backgroundColor: randomColor() }}
-            >
-              {item.percentage}%
-            </span>
+          <li
+            key={item.id}
+            className={css.item}
+            style={{ backgroundColor: randomColor() }}
+          >
+            <span className={css.label}>{item.label}</span>
+            <span className={css.percentage}>{item.percentage}%</span>
           </li>
         ))}
       </ul>
     </section>
-    )
-}
-
-
+  );
+};
